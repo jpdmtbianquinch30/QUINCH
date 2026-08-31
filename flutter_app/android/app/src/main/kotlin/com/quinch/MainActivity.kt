70ee4588ceeafb84e161b5136b891df4ae179531
Jean-Philippe Bianquinch <jeanphilippebianquinch@gmail.com>
2026-08-31 01:27:16 +0000

    Pause Flutter: deplace sur branche flutter-paused, focus 100% Angular

--- flutter_app/android/app/src/main/kotlin/com/quinch/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.quinch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-// This is a basic Flutter widget test.
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App starts', (WidgetTester tester) async {
-    // Basic test placeholder
-    expect(1 + 1, equals(2));
-  });
-}
